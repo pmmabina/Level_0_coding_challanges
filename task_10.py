f5e456746a5ff861a17cb99bcf10c60375a22511
puledim <puledim@bcx.co.za>
2022-02-01 02:54:30 +0200

    updated task_09 and task_10 to use parameters as input

--- task_10.py
+++ task_10.py
@@ -1,9 +1,5 @@
-def common_charectors():
-    first_string = input("Enter the first string ")
-    second_string = input("Enter the second string ")
-    
+def common_charectors(first_string, second_string):
     for i in first_string:
         if i in second_string:
             print(i, end = ", ")
-            
-common_charectors()
+common_charectors("computers", "house")
